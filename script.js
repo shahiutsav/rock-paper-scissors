799ddc86ca118ee53b2845f842c8d6a4b02b80c4
Utsav Shahi <shahiutsav180@gmail.com>
2022-03-18 09:56:14 +0545

    Take user input through prompt

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 // create list of "Rock, Paper, Scissors" for computer to choose from
 const computerArmory = ["Rock", "Paper", "Scissors"]
+
 // Randomly choose an object from the list
 function computerPlay() {
     const computerWeapon =
         computerArmory[Math.floor(Math.random() * computerArmory.length)]
 
     console.log(computerWeapon)
+    return computerWeapon
 }
 
 computerPlay()
+
 // take user input
+const playerWeapon = prompt("Please enter your 'weapon': ")
+
 // compare the user input with computer's selection
 // Declare the result
