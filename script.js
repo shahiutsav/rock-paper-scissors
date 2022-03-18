9f83b71381d1fde6479353cbb7e17a410fc1839e
Utsav Shahi <shahiutsav180@gmail.com>
2022-03-18 09:51:21 +0545

    Create list of rock, paper, scissors

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // create list of "Rock, Paper, Scissors" for computer to choose from
+const computerArmory = ["Rock", "Paper", "Scissors"]
 // Randomly choose an object from the list
 // take user input
 // compare the user input with computer's selection
