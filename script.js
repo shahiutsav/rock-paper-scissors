da290d98b5635fbf5e60e1913a3744936c9e7fe9
Utsav Shahi <shahiutsav180@gmail.com>
2022-03-18 10:17:33 +0545

    Add algorithm for comparing selection

--- script.js
+++ script.js
@@ -15,6 +15,21 @@ playerWeapon = playerInput.toLowerCase()
 function compareWeapon(computerWeapon, playerWeapon) {
     console.log(computerWeapon)
     console.log(playerWeapon)
+    if (computerWeapon == "scissors" && playerWeapon == "paper") {
+        console.log("You lose! Scissors beats Paper")
+    } else if (computerWeapon == "rock" && playerWeapon == "scissors") {
+        console.log("You lose! Rock beats Scissors")
+    } else if (computerWeapon == "paper" && playerWeapon == "rock") {
+        console.log("You lose! Paper beats Rock")
+    } else if (computerWeapon == "paper" && playerWeapon == "scissors") {
+        console.log("You Win! Scissors beats Paper")
+    } else if (computerWeapon == "scissors" && playerWeapon == "rock") {
+        console.log("You Win! Rock beats Scissors")
+    } else if (computerWeapon == "rock" && playerWeapon == "paper") {
+        console.log("You Win! Paper beats Rock")
+    } else {
+        console.log("It's a draw")
+    }
 }
 
 compareWeapon(computerPlay(), playerWeapon)
