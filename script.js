8c33c774cebc78c84365d26da880335cc3d21c62
Utsav Shahi <shahiutsav180@gmail.com>
2022-03-18 10:04:26 +0545

    Change order of functions and variables
    Move playerInput inside compareWeapon function

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 // create list of "Rock, Paper, Scissors" for computer to choose from
-const computerArmory = ["Rock", "Paper", "Scissors"]
+const computerArmory = ["rock", "paper", "scissors"]
 
 // Randomly choose an object from the list
 function computerPlay() {
     const computerWeapon =
         computerArmory[Math.floor(Math.random() * computerArmory.length)]
-
-    console.log(computerWeapon)
     return computerWeapon
 }
 
-computerPlay()
-
-// take user input
-const playerWeapon = prompt("Please enter your 'weapon': ")
-
 // compare the user input with computer's selection
+function compareWeapon(computerWeapon, playerWeapon) {
+    computerWeapon = computerPlay()
+    playerInput = prompt("Please enter your 'weapon': ")
+    playerWeapon = playerInput.toLowerCase()
+    console.log(computerWeapon)
+    console.log(playerWeapon)
+}
+
+compareWeapon()
 // Declare the result
