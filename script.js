db20938085ae56eae8654e62201e1b6e6fdd0a1c
Utsav Shahi <shahiutsav180@gmail.com>
2022-03-18 09:53:34 +0545

    Select random object from computerArmory list

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // create list of "Rock, Paper, Scissors" for computer to choose from
 const computerArmory = ["Rock", "Paper", "Scissors"]
 // Randomly choose an object from the list
+function computerPlay() {
+    const computerWeapon =
+        computerArmory[Math.floor(Math.random() * computerArmory.length)]
+
+    console.log(computerWeapon)
+}
+
+computerPlay()
 // take user input
 // compare the user input with computer's selection
 // Declare the result
