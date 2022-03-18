1015f0aaf1c15cbf6e078cefab270e31a3c1cc62
Utsav Shahi <shahiutsav180@gmail.com>
2022-03-18 09:49:19 +0545

    Create homepage with script enabled
    Create index.html and script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// create list of "Rock, Paper, Scissors" for computer to choose from
+// Randomly choose an object from the list
+// take user input
+// compare the user input with computer's selection
+// Declare the result
