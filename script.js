320152df12375c81d5e9778977ccd8d0393fb467
Utsav Shahi <shahiutsav180@gmail.com>
2022-03-18 10:20:31 +0545

    Return values instead of console.log

--- script.js
+++ script.js
@@ -17,18 +17,25 @@ function compareWeapon(computerWeapon, playerWeapon) {
     console.log(playerWeapon)
     if (computerWeapon == "scissors" && playerWeapon == "paper") {
         console.log("You lose! Scissors beats Paper")
+        return "You lose! Scissors beats Paper"
     } else if (computerWeapon == "rock" && playerWeapon == "scissors") {
         console.log("You lose! Rock beats Scissors")
+        return "You lose! Rock beats Scissors"
     } else if (computerWeapon == "paper" && playerWeapon == "rock") {
         console.log("You lose! Paper beats Rock")
+        return "You lose! Paper beats Rock"
     } else if (computerWeapon == "paper" && playerWeapon == "scissors") {
         console.log("You Win! Scissors beats Paper")
+        return "You Win! Scissors beats Paper"
     } else if (computerWeapon == "scissors" && playerWeapon == "rock") {
         console.log("You Win! Rock beats Scissors")
+        return "You Win! Rock beats Scissors"
     } else if (computerWeapon == "rock" && playerWeapon == "paper") {
         console.log("You Win! Paper beats Rock")
+        return "You Win! Paper beats Rock"
     } else {
         console.log("It's a draw")
+        return "It's a draw"
     }
 }
 
