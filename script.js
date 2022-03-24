8a5638898d1171bc8461f689a398a351afdce6f9
Utsav Shahi <shahiutsav180@gmail.com>
2022-03-24 09:07:36 +0545

    Make comments informative

--- script.js
+++ script.js
@@ -5,60 +5,63 @@ const computerScoreDisplay = document.querySelector("#computerScore")
 const scoreDisplay = document.querySelector(".score-comparison")
 const buttons = document.querySelector(".buttons")
 
-// create list of "Rock, Paper, Scissors" for computer to choose from
+// Create list of "Rock, Paper, Scissors" for computer to choose from
 const computerArmory = ["rock", "paper", "scissors"]
 
-// Initial scores for both
+// Initial score for both computer and player
 let computerScore = 0
 let playerScore = 0
 
-// Randomly choose an object from the list
+// Randomly choose and return a hand from the list
 function computerPlay() {
     const computerSelection =
         computerArmory[Math.floor(Math.random() * computerArmory.length)]
     return computerSelection
 }
 
-// compare the user input with computer's selection
+// Compare user's selection with computer's selection
 function compareSelection(computerSelection, playerSelection) {
     // Draw case scenario
     if (computerSelection == playerSelection) {
         return "It's a draw this round"
-    } // every time computer wins
+    } // scenarios where the computer wins
     else if (
         (computerSelection == "scissors" && playerSelection == "paper") ||
         (computerSelection == "rock" && playerSelection == "scissors") ||
         (computerSelection == "paper" && playerSelection == "rock")
     ) {
-        computerScore += 1
+        computerScore += 1 // Adds to the computer score
         return `Oh oh! Your ${capitalize(
             playerSelection
         )} got beat by opponent's ${capitalize(computerSelection)}`
-    } // every user wins
+    } // scenarios where the user wins
     else if (
         (computerSelection == "paper" && playerSelection == "scissors") ||
         (computerSelection == "scissors" && playerSelection == "rock") ||
         (computerSelection == "rock" && playerSelection == "paper")
     ) {
-        playerScore += 1
+        playerScore += 1 // Adds to the player score
         return `Great! Your ${capitalize(
             playerSelection
         )} annihilated your opponent's ${capitalize(computerSelection)}`
     }
 }
 
-// repeat the game for five rounds
+// Main function to be called
 function game(playerSelection) {
-    scoreInfo.textContent = compareSelection(computerPlay(), playerSelection)
+    scoreInfo.textContent = compareSelection(computerPlay(), playerSelection) // compare the selections
     if (playerScore === 5 || computerScore === 5) {
-        replayButton()
+        // check if player or computer has 5 points/score
+        replayButton() // Add replay button
         if (playerScore > computerScore) {
+            // Player win scenario
             scoreDisplay.textContent = "You Won!"
             scoreInfo.classList.add("win")
             scoreInfo.textContent = `Great going! You won by ${
                 playerScore - computerScore
             } point(s)`
         } else {
+            // Computer win scenario
             scoreDisplay.textContent = "Game Over!"
             scoreInfo.classList.add("lost")
             scoreInfo.textContent = `Bummer! You lost by ${
@@ -66,6 +69,7 @@ function game(playerSelection) {
             } point(s)`
         }
     } else {
+        // Display ongoing score
         playerScoreDisplay.textContent = "Player Score: " + playerScore
         computerScoreDisplay.textContent = "Computer Score: " + computerScore
     }
@@ -73,15 +77,16 @@ function game(playerSelection) {
 
 // Display replay button
 function replayButton() {
-    const btnRestart = document.createElement("button")
-    btnRestart.classList.add("btn-replay")
-    btnRestart.textContent = "Replay"
+    const btnReplay = document.createElement("button") // Create replay button element
+    btnReplay.classList.add("btn-replay") // Add style to the button
+    btnReplay.textContent = "Replay" // Add content inside of the button
 
     while (buttons.firstChild) {
-        buttons.removeChild(buttons.lastChild)
+        // run until the first child has been removed
+        buttons.removeChild(buttons.lastChild) // removing existing buttons from the parent node `buttons`
     }
-    buttons.appendChild(btnRestart)
-    btnRestart.onclick = () => location.reload()
+    buttons.appendChild(btnReplay) // Add button to html document
+    btnReplay.onclick = () => location.reload() // Reload page on click
 }
 
 // Capitalize strings
